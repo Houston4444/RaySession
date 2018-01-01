1e6384a3033ffe7cb04bafe45f84ab8e482d7078
houston <picotmathieu@gmail.com>
2018-01-01 23:35:20 +0100

    put debug in server thread

--- src/nsm_client.py
+++ src/nsm_client.py
@@ -2,11 +2,6 @@
 
 from PyQt5.QtCore import QObject, pyqtSignal
 from liblo import ServerThread, make_method
-#from shared import *
-
-def ifDebug(string):
-    if debug:
-        print(string, file=sys.stderr)
         
 class NSMSignaler(QObject):
     server_sends_open = pyqtSignal(str, str, str)
@@ -15,33 +10,32 @@ class NSMSignaler(QObject):
     hide_optional_gui = pyqtSignal()
 
 class NSMThread(ServerThread):
-    def __init__(self, name, signaler, bool_debug):
+    def __init__(self, name, signaler, debug):
         ServerThread.__init__(self)
         self.name     = name
         self.signaler = signaler
-        
-        global debug
-        debug = bool_debug
+        self.debug    = debug
 
     @make_method('/nsm/client/open', 'sss')
     def nsmClientOpen(self, path, args):
-        ifDebug('serverOSC::%s_receives %s, %s' % (self.name, path, str(args)))
+        self.ifDebug('serverOSC::%s_receives %s, %s' % (self.name, path, str(args)))
         self.signaler.server_sends_open.emit(*args)
     
     @make_method('/nsm/client/save', '')
     def nsmClientSave(self, path, args):
-        ifDebug('serverOSC::%s_receives %s, %s' % (self.name, path, str(args)))
+        self.ifDebug('serverOSC::%s_receives %s, %s' % (self.name, path, str(args)))
         self.signaler.server_sends_save.emit()
         
     @make_method('/nsm/client/show_optional_gui', '')
     def nsmClientShow_optional_gui(self, path, args):
-        ifDebug('serverOSC::%s_receives %s, %s' % (self.name, path, str(args)))
+        self.ifDebug('serverOSC::%s_receives %s, %s' % (self.name, path, str(args)))
         self.signaler.show_optional_gui.emit()
 
     @make_method('/nsm/client/hide_optional_gui', '')
     def nsmClientHide_optional_gui(self, path, args):
-        ifDebug('serverOSC::%s_receives %s, %s' % (self.name, path, str(args)))
+        self.ifDebug('serverOSC::%s_receives %s, %s' % (self.name, path, str(args)))
         self.signaler.hide_optional_gui.emit()
-    
-    
-     
+        
+    def ifDebug(self, string):
+        if self.debug:
+           print(string, file=sys.stderr) 
