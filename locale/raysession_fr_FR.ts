3178598523d06a0d6aee9e55816d52f0c29e797c
houston <picotmathieu@gmail.com>
2018-01-22 23:01:24 +0100

    add TRANSLATORS infos

--- locale/raysession_fr_FR.ts
+++ locale/raysession_fr_FR.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="fr_FR">
+<!DOCTYPE TS><TS version="2.0" language="fr_FR" sourcelanguage="">
 <context>
     <name>@default</name>
     <message>
