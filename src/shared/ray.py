97a7b527474e91c75b6cd929c3859deadf741459
houston <picotmathieu@gmail.com>
2019-07-24 08:53:30 +0200

    change version to 0.8.0

--- src/shared/ray.py
+++ src/shared/ray.py
@@ -24,7 +24,7 @@ if QT_VERSION < (5, 6):
         + "You won't be warned if a process can't be launch.\n")
 
 # Ray Session version
-VERSION = "0.7.2"
+VERSION = "0.8.0"
 
 APP_TITLE = 'Ray Session'
 
