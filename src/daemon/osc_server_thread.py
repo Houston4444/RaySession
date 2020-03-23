c17db9019f627f674fb9cfb3c0ebec9ed0722111
houston <picotmathieu@gmail.com>
2020-03-23 14:46:52 +0100

    add 'set_options' to ray_control

--- src/daemon/osc_server_thread.py
+++ src/daemon/osc_server_thread.py
@@ -939,16 +939,52 @@ class OscServerThread(ClientCommunicating):
         pass
     
     @ray_method('/ray/trashed_client/restore', 's')
-    def rayGuiTrashRestore(self, path, args, types, src_addr):
+    def rayTrashedClientRestore(self, path, args, types, src_addr):
         pass
         
     @ray_method('/ray/trashed_client/remove_definitely', 's')
-    def rayGuiTrashRemoveDefinitely(self, path, args, types, src_addr):
+    def rayTrashedClientRemoveDefinitely(self, path, args, types, src_addr):
         pass
     
     @ray_method('/ray/net_daemon/duplicate_state', 'f')
     def rayDuplicateState(self, path, args, types, src_addr):
         pass
+    
+    @ray_method('/ray/server/set_options', None)
+    def rayServerSetOptions(self, path, args, types, src_addr):
+        if not ray.areTheyAllString(args):
+            return False
+        
+        for option in args:
+            option_value = True
+            if option.startswith('not_'):
+                option_value = False
+                option = option.replace('not_', '', 1)
+            
+            if option == 'save_from_client':
+                self.option_save_from_client = option_value
+            elif option == 'bookmark_session_folder':
+                self.option_bookmark_session = option_value
+            elif option == 'desktops_memory':
+                if option_value and not self.option_has_wmctrl:
+                    self.send(src_addr, '/minor_error', path,
+                        "wmctrl is not present. Impossible to activate 'desktops_memory' option")
+                    continue
+                self.option_desktops_memory = option_value
+            elif option == 'snapshots':
+                if option_value and not self.option_has_git:
+                    self.send(src_addr, '/minor_error', path,
+                        "git is not present. Impossible to activate 'snapshots' option")
+                    continue
+                self.option_snapshots = option_value
+                
+        options = self.getOptions()
+        
+        for gui_addr in self.gui_list:
+            if not ray.areSameOscPort(gui_addr.url, src_addr.url):
+                self.send(gui_addr, '/ray/gui/server/options', options)
+                
+        self.send(src_addr, '/reply', path, 'Options set')
     
     @ray_method('/ray/option/save_from_client', 'i')
     def rayOptionSaveFromClient(self, path, args, types, src_addr):
