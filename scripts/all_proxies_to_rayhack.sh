3d8e6ffa60407d63ca0321a30bab844c44fd133e
houston <picotmathieu@gmail.com>
2020-10-28 01:04:49 +0100

    script to convert ray-proxy client to Ray-Hack

--- scripts/all_proxies_to_rayhack.sh
+++ scripts/all_proxies_to_rayhack.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+print_help(){
+    echo "usage: $1 arguments
+    where options can be
+        --help          print this help
+        all_sessions    convert ray-proxy clients to RayHack in all sessions
+        all_templates   convert all ray-proxy templates to Ray-Hack
+        running_client CLIENT_ID
+                convert running CLIENT_ID to ray_hack if it is a ray-proxy
+"
+}
+
 proxy_to_ray_hack(){
     client_id="$1"
     echo "  treating $client_id"
@@ -20,11 +31,18 @@ proxy_to_ray_hack(){
     echo "remove_definitely $client_id"
     ray_control trashed_client $client_id remove_definitely
     
+    normal_lines=$(ray_control client $client_id get_properties|grep -e ^label: -e ^description: -e ^icon: -e ^desktop_file:)
     exec_line=$(echo "$proxy_properties"|grep ^executable:)
-    good_lines=$(echo "$proxy_properties"|grep -e ^arguments: -e ^no_save_level: -e ^config_file:)
+    good_lines=$(echo "$proxy_properties"|grep -e ^arguments: -e ^no_save_level:)
     save_sig_line=$(echo "$proxy_properties"|grep ^save_signal:)
     stop_sig_line=$(echo "$proxy_properties"|grep ^stop_signal:)
     wait_win_line=$(echo "$proxy_properties"|grep ^wait_window:)
+    config_file_line=$(echo "$proxy_properties"|grep ^config_file:)
+
+    config_file="${config_file_line#*:}"
+    if [ -z "$config_file" ] && [[ "${save_sig_line#*:}" != "0" ]];then
+        config_file=anything
+    fi
 
     # mmmh, on very old proxies, stop signal was always saved to SIGUSR1
     stop_sig=${stop_sig_line#*:}
@@ -34,9 +52,11 @@ proxy_to_ray_hack(){
     if [ -n "$new_client_id" ];then
         echo "set properties of new client '$new_client_id'"
         ray_control client $new_client_id set_properties "$good_lines
+config_file:${config_file}
 save_sig:${save_sig_line#*:}
 stop_sig:$stop_sig
-wait_win:${wait_win_line#*:}"
+wait_win:${wait_win_line#*:}
+$normal_lines"
         rm -R "$c_project_path"
     else
         echo "Add executable failed, sorry, no more client"
@@ -46,6 +66,8 @@ wait_win:${wait_win_line#*:}"
 }
 
 all_sessions(){
+    IFS=$'\n'
+    
     for session in $(ray_control list_sessions);do
         echo "____"
         echo "session:"$session
@@ -59,12 +81,50 @@ all_sessions(){
     ray_control save
 }
 
+all_templates(){
+    ray_control open_session_off $(mktemp -u)
 
+    IFS=$'\n'
 
+    for client_template in $(ray_control list_user_client_templates);do
+        echo "  client_template:$client_template"
+        client_id=$(ray_control add_user_client_template "$client_template" not_start) || continue
+        echo "  client_id:$client_id"
+        if ray_control client $client_id get_properties|grep ^executable:ray-proxy;then
+            echo "     ray-proxy -> Ray-Hack"
+            proxy_to_ray_hack $client_id
+            echo "      save as template $client_template"
+            ray_control client $client_id save_as_template "$client_template"
+        fi
+        ray_control client $client_id trash
+        ray_control trashed_client $client_id remove_definitely
+    done
+}
 
-export RAY_CONTROL_PORT=$(ray_control start_new_hidden)
-
-IFS=$'\n'
-
-all_sessions
-ray_control quit
+argument=$1
+shift
+case $argument in
+    all_sessions|all_templates)
+        export RAY_CONTROL_PORT=$(ray_control start_new_hidden)
+        $argument
+        ray_control quit
+    ;;
+    running_client)
+        client_id="$1"
+        [ -z "$client_id" ] && print_help && exit 1
+        echo -n "session_path:"
+        ray_control get_session_path || exit 1
+        if ! ray_control list_clients|grep ^$client_id$;then
+            echo "no client with $client_id client_id"
+            exit 1
+        fi
+        ray_control client "$client_id" stop
+        proxy_to_ray_hack "$client_id"
+    ;;
+    --help )
+        print_help
+    ;;
+    *)
+        print_help
+        exit 1
+esac
