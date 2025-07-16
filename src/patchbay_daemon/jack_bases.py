015b27aa4ce50af2e7533d1d2fadd53226236f9f
Houston4444 <picotmathieu@gmail.com>
2025-07-16 14:41:29 +0200

    use patch event queue (still need to write datas in the main thread)

---
+++ src/patchbay_daemon/jack_bases.py
@@ -0,0 +1,76 @@
+from dataclasses import dataclass
+from enum import IntEnum, Enum, auto
+from queue import Queue
+import time
+from typing import Iterator, TypeAlias
+
+
+PatchEventArg: TypeAlias = str | tuple[str, str] | tuple[int, str, str]
+
+
+@dataclass
+class TransportPosition:
+    frame: int
+    rolling: bool
+    valid_bbt: bool
+    bar: int
+    beat: int
+    tick: int
+    beats_per_minutes: float
+
+
+class TransportWanted(IntEnum):
+    NO = 0
+    'do not send any transport info'
+    
+    STATE_ONLY = 1
+    'send transport info only when play/pause changed'
+
+    FULL = 2
+    'send all transport infos'
+
+
+class PatchEvent(Enum):
+    CLIENT_ADDED = auto()
+    CLIENT_REMOVED = auto()
+    PORT_ADDED = auto()
+    PORT_REMOVED = auto()
+    PORT_RENAMED = auto()
+    CONNECTION_ADDED = auto()
+    CONNECTION_REMOVED = auto()
+    METADATA_CHANGED = auto()
+
+
+class PatchEventQueue(Queue[tuple[PatchEvent, PatchEventArg, float]]):
+    def __init__(self, maxsize: int = 0):
+        super().__init__(maxsize)
+        self.oldies_queue = Queue()
+        
+    def add(self, *args):
+        nargs = len(args)
+        if nargs > 2:
+            event, *rest = args
+        elif nargs == 2:
+            event, rest = args
+        elif nargs == 1:
+            event = args[0]
+            rest = None
+        else:
+            raise TypeError
+        self.put((event, rest))
+        self.oldies_queue.put((event, rest, time.time()))
+        
+    def __iter__(self) -> Iterator[tuple[PatchEvent, PatchEventArg]]:
+        while self.qsize():
+            event, event_arg = self.get()
+            yield (event, event_arg)
+    
+    def oldies(self, required_time: float = 0.200) \
+            -> Iterator[tuple[PatchEvent, PatchEventArg]]:
+        '''Iter only events older than required_time'''
+        while self.oldies_queue.qsize():
+            if time.time() - self.oldies_queue.queue[0][2] < required_time:
+                break
+
+            event, event_arg, time_ = self.oldies_queue.get()
+            yield (event, event_arg)
