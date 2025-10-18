ae6ee318c13e1369b83acfeddbf2ab5ca7c1e02b
Houston4444 <picotmathieu@gmail.com>
2025-10-18 20:17:29 +0200

    remove a '/'

--- src/gui/gui_tools.py
+++ src/gui/gui_tools.py
@@ -101,12 +101,12 @@ def ray_icon(icon_name: str, dark=False) -> QIcon:
     
     icon = QIcon()
     breeze = 'breeze-dark' if dark else 'breeze'
-    icon.addFile(f'scalable:/{breeze}/{icon_name}', QSize(22, 22))
+    icon.addFile(f'scalable:{breeze}/{icon_name}', QSize(22, 22))
     # icon.addPixmap(
     #     QPixmap(f'scalable:/{breeze}/{icon_name}'),
     #     QIcon.Mode.Normal, QIcon.State.On)
     icon.addPixmap(
-        QPixmap(f'scalable:/{breeze}/disabled/{icon_name}'),
+        QPixmap(f'scalable:{breeze}/disabled/{icon_name}'),
         QIcon.Mode.Disabled, QIcon.State.Off)
     
     from qtpy.QtCore import QDir
