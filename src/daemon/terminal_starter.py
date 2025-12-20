b213c7c7ea88065fbc4f4d7b5f55399e5abacb08
Houston4444 <picotmathieu@gmail.com>
2025-12-20 08:30:21 +0100

    if elif to case

--- src/daemon/terminal_starter.py
+++ src/daemon/terminal_starter.py
@@ -2,46 +2,44 @@ import os
 import shutil
 
 def which_terminal(title='', hold=True) -> list[str]:
-        '''return the most appropriate terminal executable
-        with its arguments'''
-        terminals = ['gnome-terminal', 'mate-terminal', 'xfce4-terminal',
-                     'xterm', 'konsole', 'lxterminal', 'rxvt']
-        current_desktop = os.getenv('XDG_CURRENT_DESKTOP')
-        terminal = ''
+    '''return the most appropriate terminal executable
+    with its arguments'''
+    terminals = ['gnome-terminal', 'mate-terminal', 'xfce4-terminal',
+                 'xterm', 'konsole', 'lxterminal', 'rxvt']
+    current_desktop = os.getenv('XDG_CURRENT_DESKTOP')
+    terminal = ''
 
-        # make prior most appropriate terminal
-        if current_desktop == 'GNOME':
+    # make prior most appropriate terminal
+    match current_desktop:
+        case 'GNOME':
             pass
-
-        elif current_desktop == 'KDE':
+        case 'KDE':
             terminals.remove('konsole')
             terminals.insert(0, 'konsole')
-
-        elif current_desktop == 'MATE':
+        case 'MATE':
             terminals.remove('mate-terminal')
             terminals.insert(0, 'mate-terminal')
-
-        elif current_desktop == 'XFCE':
+        case 'XFCE':
             terminals.remove('xfce4-terminal')
             terminals.insert(0, 'xfce4-terminal')
             terminals.insert(0, 'xfce-terminal')
-
-        elif current_desktop == 'LXDE':
+        case 'LXDE':
             terminals.remove('lxterminal')
             terminals.insert(0, 'lxterminal')
 
-        # search executable for terminals
-        for term in terminals:
-            if shutil.which(term):
-                terminal = term
-                break
-        else:
-            return list[str]()
+    # search executable for terminals
+    for term in terminals:
+        if shutil.which(term):
+            terminal = term
+            break
+    else:
+        return list[str]()
 
-        if terminal == 'gnome-terminal':
+    match terminal:
+        case 'gnome-terminal':
             return [terminal, '--hide-menubar', '--wait', '--']
 
-        if terminal == 'konsole':
+        case 'konsole':
             base_args = [terminal, '--hide-tabbar', '--hide-menubar']
             if hold:
                 base_args.append('--hold')
@@ -53,18 +51,18 @@ def which_terminal(title='', hold=True) -> list[str]:
             
             return base_args
 
-        if terminal == 'mate-terminal':
+        case 'mate-terminal':
             if title:
                 return [terminal, '--hide-menubar', '--disable-factory',
                         '--title', title, '--']
 
             return [terminal, '--hide-menubar', '--disable-factory', '--']
 
-        if terminal == 'xfce4-terminal':
+        case 'xfce4-terminal':
             if title:
                 return [terminal, '--hide-menubar', '--hide-toolbar',
                         '-T', title, '-e']
 
             return [terminal, '--hide-menubar', '--hide-toolbar', '-e']
 
-        return [terminal, '-e']
+    return [terminal, '-e']
